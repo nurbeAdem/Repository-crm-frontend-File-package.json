8333553903479bba609f59ca64b33b14e374ab49
nurbeAdem <nureddin.albeshir@gmail.com>
2025-11-21 19:22:04 +0300

    Update store.js

--- File: src/File: src/store.js
+++ File: src/File: src/store.js
@@ -1,1 +1,8 @@
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+import leadsReducer from './features/leads/leadsSlice';
 
+
+const rootReducer = combineReducers({ leads: leadsReducer });
+const store = createStore(rootReducer, applyMiddleware(thunk));
+export default store;
